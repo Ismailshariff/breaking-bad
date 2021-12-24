910b47d546223428e6a78c0cd49cf2e6d163adc5
Ismailshariff <shariffshariff00@gmail.com>
2021-12-24 21:54:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import Card from "./components/card"
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    breakingbadPromise();
+  }, []);
+
+  const breakingbadPromise = () => {
+    fetch("https://www.breakingbadapi.com/api/characters")
+      .then((response) => response.json())
+      .then((recivedData) => setData(recivedData));
+  };
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    
+      <h1>Breaking Bad Characters</h1>
+      <div className="box">
+      {data.map((user) => (
+       <div>
+         <Card src={user.img} name={user.name} birthday={user.birthday} portrayed={user.portrayed}/>
+       </div>
+      ))}
+      </div>
     </div>
   );
 }
